c8311a20e1b7e6bffd75b5c7ebe8ad36533fac12
JeetPatel301095 <43115993+JeetPatel301095@users.noreply.github.com>
2019-12-05 17:12:43 -0500

    New python files

--- IOTProject629/routes/index.js
+++ IOTProject629/routes/index.js
@@ -1,22 +1,65 @@
 var PythonShell = require('python-shell');
+var a =false;
+var b;
+
 
 const constructormethod = app =>{
     app.get("/",async (req, res)=>{
         console.log("Hello");
-        var options = {
-            mode: 'text',
-            pythonOptions: ['-u'], // get print results in real-time
-            scriptPath: 'C:\\Users\\Jeet\\Jeet\\EE-629\\EE-629-IOT\\IOTProject629\\routes\\',
-            args: [5,4]
-          }
-        PythonShell.PythonShell.run('hello.py',options,function(err,result){
-            if(err) throw err;
-            console.log(result);
-            console.log('finished');
-            res.render("temp/basic");
-        });        
+        res.render("temp/basic")
     });
 
+    app.post("/on",async(req,res)=>{
+        console.log("On");
+        if(a)
+            res.send("LED is already On");
+        else
+        {
+            a=true;
+            b = Date.now();       
+            var options = {
+                mode: 'text',
+                encoding:'utf8',
+                pythonOptions: ['-u'],
+                scriptPath: "./",
+            }
+            
+            var test = new PythonShell.PythonShell("C:\\Users\\Jeet\\Jeet\\EE-629\\EE-629-IOT\\IOTProject629\\routes\\on.py",options);
+            test.on("message",function(message){
+                res.send(message);
+            })
+        }
+        
+    });
+
+    
+    app.post("/off",async(req,res)=>{
+        console.log("Off");
+        if(a)
+        {
+            a= false;
+            var c= Date.now()
+            var d = c-b;
+            d/=1000;
+            console.log(d);
+            var options = {
+                mode: 'text',
+                encoding:'utf8',
+                pythonOptions: ['-u'],
+                scriptPath: "./",
+            }
+            
+            var test = new PythonShell.PythonShell("C:\\Users\\Jeet\\Jeet\\EE-629\\EE-629-IOT\\IOTProject629\\routes\\off.py",options);
+            test.on("message",function(message){
+                res.send(message+"  "+d);
+            })
+        }
+        else
+        {
+            res.send("LED is already Off");
+        }
+        
+    });
 };
 
 module.exports = constructormethod;
